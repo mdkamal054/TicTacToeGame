aeb24e06241e640a6fa5d11ca973635c39003242
osamakamal <info.osamakamal@gmail.com>
2023-09-03 00:23:47 +0530

    Initial Commit

---
+++ TicToeGame/TicToeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicToeGameApp.swift
+//  TicToeGame
+//
+//  Created by Osama Kamal on 03/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TicToeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
